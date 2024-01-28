b9530cbb9d3bb785b7ad5c180a6a8f94c35371b0
Armynerh <aimenah@outlook.com>
2024-01-27 19:03:58 -0500

    modified App.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Home from './components/Home';
 import NavBar from './components/NavBar';
+import MediaIcons from './components/MediaIcons';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <MediaIcons />
     </div>
   );
 }
