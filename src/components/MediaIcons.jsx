9d116b5fdb9a7b6111dcf9abfde99ec3bc4a696e
Armynerh <aimenah@outlook.com>
2024-01-27 19:05:33 -0500

    create files for social media Icons

---
+++ src/components/MediaIcons.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BsGithub, BsLinkedin, BsFillPersonLinesFill } from 'react-icons/bs';
+import { SiMinutemailer } from 'react-icons/si';
+
+const MediaIcons = () => (
+  <div className=" flex flex-col top-[35%] left-0 fixed">
+    <ul>
+      <li className="flex justify-between items-center w-40 h-14 px-4 ml- bg-">
+        <a href="" className="flex justify-between item-center w-full text-white">
+          <>
+          </>
+        </a>
+      </li>
+    </ul>
+  </div>
+);
+
+export default MediaIcons;
