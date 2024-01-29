dbd79f00d4c2bc34ef244dc7c59c784afddc87b6
Armynerh <aimenah@outlook.com>
2024-01-29 00:52:58 -0500

    add about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from './components/Home';
 import NavBar from './components/NavBar';
 import MediaIcons from './components/MediaIcons';
+import About from './components/About';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavBar />
       <Home />
       <MediaIcons />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const About = () => (
+  <div name="about" className=" w-full h-screen text-teal-950 ">
+    <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="bg-teal-950 p-8 mb-7">
+        <p className="text-white text-5xl font-extrabold inline border-b-4 border-gray-400">About Me</p>
+      </div>
+      {/* eslint-disable */}
+      <p>
+        As I embark on the exciting adventure of full-stack software development, I find myself navigating the worlds of technology and education with ease. As an enthusiastic and dynamic professional, I am now working as a freelance full-stack developer. I have engaged in 100+ hours of remote pair programming with international peers, Implemented unit and integration tests with Jest, Unittest, Pytest and RSpec, achieving 95% test coverage, developed responsive and dynamic user interfaces using React.js, Implemented server-side logic and API integrations with Ruby on Rails and Flask, and designed and maintained relational databases using SQL (PostgreSQL, MySQL)
+      </p>
+      <br />
+      <p>
+        Beyond the lines of code that I write, my story also delves into the world of teaching English as a Second Language. With a TEFL/IDP certification and more than three years of experience as an ESL/IELTS trainer, I&lsquo;ve discovered the perfect blend between linguistic refinement and technological competence.
+      </p>
+      <br />
+      <p>
+        In terms of my career goals, I see myself not only as a programmer, but also as a future expert in tech curriculum creation and instruction. My motivation is driven by a desire to contribute to companies that are shaping the future of learning, developing digital landscapes in which information grows seamlessly.
+
+      </p>
+      <br />
+      <p>
+
+        Beyond the computer and the classroom, I&lsquo;m usually engaged in the pages of a book, mesmerised by podcasts, studying the world through documentaries, travelling the world, or immersed in the strategic landscapes of games, every aspect of my life contributes to an ever-changing story.
+        I am on a mission to illuminate the intersection between technology and education, to create a narrative that goes beyond code and classrooms.
+      </p>
+      <br />
+      <p>
+        I love talk about the fascinating connections between technology and education. I&lsquo;m always ready to engage in conversations about coding, the latest in ReactJs, or researching potential career routes. I&lsquo;m look forward to meeting with like-minded people and exploring opportunities that match my experience. Send me a message, and let us go on this journey together!
+      </p>
+      
+    </div>
+  </div>
+);
+
+export default About;
